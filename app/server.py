fe05113d2dda88dd69eabe0b1683688f80b28b6d
Peter Zhu <zhudanmeng@gmail.com>
2021-01-31 00:36:02 -0400

    2

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-model_file_url = 'https://drive.google.com/uc?export=download&id=1Vq22Czo7zQx6oJ2d-zFftrC5kpMMr-kq'
+model_file_url = ''
 model_file_name = 'stage-1.pth'
 classes = ['dirty', 'clean']
 path = Path(__file__).parent
